1f37151b61936f8bcb6621112bc7e1c1a3d017c9
mathoshi <47573361+mathoshi@users.noreply.github.com>
2019-05-06 21:43:23 -0400

    Update scene2.lua

--- scene2.lua
+++ scene2.lua
@@ -3,7 +3,7 @@
 -- Created by: Matsuru Hoshi
 -- Created on: May 6, 2019
 --
--- This file plays with scenes
+-- This file plays with scene 2
 -----------------------------------------------------------------------------------------
 
 local composer = require( "composer" )
@@ -33,6 +33,15 @@ end
 function scene:create( event )
  
     local sceneGroup = self.view
+    print("scene2 create")
+
+    local background  = display.newRect( display.contentCenterX, display.contentCenterY, display.contentWidth, display.contentHeight)
+    background:setFillColor( 234/255, 123/255, 33/255)
+    sceneGroup:insert(background)
+
+    local Title = display.newText( "Still. In all of the obscurity, there was a shine, buried deep down. \n You could just barely see it. And that was me.", display.contentCenterX, display.contentCenterY, "Times New Roman", 34)
+    Title:setFillColor( 1, 1, 1)
+    sceneGroup:insert(Title)
     -- Code here runs when the scene is first created but has not yet appeared on screen
  
 end
@@ -45,14 +54,7 @@ function scene:show( event )
     local phase = event.phase
  
     if ( phase == "will" ) then
-        local background  = display.setDefault( "background", 5/255, 102/255, 123/255)
-        sceneGroup:insert(background)
-
-        local Title = display.newText( "Scene 2", 160, 400, "Times New Roman", 34)
-        Title:setFillColor( 1, 1, 1)
-        sceneGroup:insert(Title)
         -- Code here runs when the scene is still off screen (but is about to come on screen)
- 
     elseif ( phase == "did" ) then
         -- Code here runs when the scene is entirely on screen
         timer.performWithDelay( 3000, showScene1)
@@ -67,6 +69,7 @@ function scene:hide( event )
     local sceneGroup = self.view
     local phase = event.phase
  
+    print("scene2 hide")
     if ( phase == "will" ) then
         -- Code here runs when the scene is on screen (but is about to go off screen)
  
@@ -79,6 +82,7 @@ end
  
 -- destroy()
 function scene:destroy( event )
+    print("scene2 destroy")
  
     local sceneGroup = self.view
     -- Code here runs prior to the removal of scene's view
@@ -89,10 +93,10 @@ end
 -- -----------------------------------------------------------------------------------
 -- Scene event function listeners
 -- -----------------------------------------------------------------------------------
-scene:addEventListener( "create", scene1 )
-scene:addEventListener( "show", scene1 )
-scene:addEventListener( "hide", scene2 )
-scene:addEventListener( "destroy", scene2 )
+scene:addEventListener( "create", scene )
+scene:addEventListener( "show", scene )
+scene:addEventListener( "hide", scene )
+scene:addEventListener( "destroy", scene )
 -- -----------------------------------------------------------------------------------
  
 return scene
